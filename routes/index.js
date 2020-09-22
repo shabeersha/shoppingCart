1d0f10daba795f69b6cee90b46cf1d0963faeba9
Shabeersha <shabeersha786786@gmail.com>
2020-09-22 23:59:24 +0530

    product listing is ready for user view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let products = [{
+    name:"iphone 11",
+    description:"Iphone 11 (64GB BLACK)",
+    image:"https://images-na.ssl-images-amazon.com/images/I/51kGDXeFZKL._SX522_.jpg",
+    category:"Mobile"
+  },
+  {
+    name:"Samsung A70s",
+    description:"Samsung A70s (128GB INDIGO)",
+    image:"https://images-na.ssl-images-amazon.com/images/I/61jkp2mBnqL._SX522_.jpg",
+    category:"Mobile"
+  },
+  {
+    name:"Macbook Pro Air",
+    description:"Apple Macbook Pro Air ",
+    image:"https://images-na.ssl-images-amazon.com/images/I/71Ae0NSObSL._SX522_.jpg",
+    category:"Laptop"
+  },
+  {
+    name:"Apple Ipad Air 2",
+    description:"Apple Ipad Air 2",
+    image:"https://images-na.ssl-images-amazon.com/images/I/718pwrPBjcL._SX522_.jpg",
+    category:"Tablet"
+  }
+]
+  res.render('index', { products });
 });
 
 module.exports = router;
