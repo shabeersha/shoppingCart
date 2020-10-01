39d1bea335d0db52c3dcf49b5411aee0fa0d52ab
Shabeersha <shabeersha786786@gmail.com>
2020-10-01 17:58:40 +0530

    connection string changed

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,11 +1,20 @@
+const collections = require('../config/collections')
+
 db=require('../config/connection')
+
 
 module.exports={
     addProduct:(product,callback)=>{
-        db.get().collection('product').insertOne(product).then((data)=>{
+        db.get().collection(collections.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
             callback(data.ops[0]._id)
         })
 
 
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products = await db.get().collection(collections.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
+        })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -5,31 +5,7 @@ var productHelpers = require('../helpers/product-helpers')
 
 /* GET admin listing. */
 router.get('/', function(req, res, next) {
-  let products = [{
-    name:"iphone 11",
-    description:"Iphone 11 (64GB BLACK)",
-    image:"https://images-na.ssl-images-amazon.com/images/I/51kGDXeFZKL._SX522_.jpg",
-    category:"Mobile"
-  },
-  {
-    name:"Samsung A70s",
-    description:"Samsung A70s (128GB INDIGO)",
-    image:"https://images-na.ssl-images-amazon.com/images/I/61jkp2mBnqL._SX522_.jpg",
-    category:"Mobile"
-  },
-  {
-    name:"Macbook Pro Air",
-    description:"Apple Macbook Pro Air ",
-    image:"https://images-na.ssl-images-amazon.com/images/I/71Ae0NSObSL._SX522_.jpg",
-    category:"Laptop"
-  },
-  {
-    name:"Apple Ipad Air 2",
-    description:"Apple Ipad Air 2",
-    image:"https://images-na.ssl-images-amazon.com/images/I/718pwrPBjcL._SX522_.jpg",
-    category:"Tablet"
-  }
-]
+  
 let findex = hbsHelpers.formatIndex;
   res.render('admin/view-products',{admin:true,products,helpers:{findex}})
 });
@@ -39,9 +15,7 @@ router.get('/add-products',function(req,res){
 })
 
 router.post('/add-products',(req,res)=>{
-  console.log(req.body)
-  console.log(req.files.images)
-
+ 
   productHelpers.addProduct(req.body,(id)=>{
     let imageFile=req.files.images
     imageFile.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
